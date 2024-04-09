8fb555841d90d8901deffa75a52ff257d81efa1f
= <=>
2024-04-09 07:25:38 +0530

    django hello world eka hari

--- members/urls.py
+++ members/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.home, name='home')
+    path('members/', views.members, name='members'),
 ]
--- members/views.py
+++ members/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('Hello world')
+def members(request):
+    return HttpResponse('Hello World')
+    
